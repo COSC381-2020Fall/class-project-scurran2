0848c846a0a0a2a16cc187bce7ad192f86332938
Sam Curran <scurran001@gmail.com>
2020-11-15 18:34:08 +0000

    Added comments and fixed syntax errors

--- serve.py
+++ serve.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
-
+# self is instance of class (like this in java). In this case web server instance
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(200) #"no problems" response
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
+        #dynamically generate webpage
         self.wfile.write(b"<!DOCTYPE html>")
         self.wfile.write(b"<html lang = 'en'>")
         self.wfile.write(b"<head>")
@@ -18,8 +19,10 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b"</html>")   
 
 port = 8080
-# 0.0.0.0 means all IP v4 addresses on local machine (?)
+# 0.0.0.0 means all IP v4 addresses on local machine 
+# i.e. if host (e.g. aws machine) has 2 ip addresses, 
+# server is reachable on both
 server_address = ("0.0.0.0", port)
 #server_address = ("127.0.0.1", port)
 httpd = HTTPServer(server_address, HTTPServer_RequestHandler)
-http.send_forever()
+httpd.serve_forever()
